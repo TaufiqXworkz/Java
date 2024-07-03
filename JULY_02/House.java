215b8754f73be3426d753d8561dec500e34b74ba
TaufiqXworkz <taufiq.xworkz@gmail.com>
2024-07-03 10:27:48 +0530

    House class updated

--- JULY_02/House.java
+++ JULY_02/House.java
@@ -1,9 +1,9 @@
 public class House {
     
-    public static void displayFamilyDetails(String... familyMembers) {
+    public static void displayFamilyDetails(String[] familyMembers) {
         System.out.println("Family Details:");
-        for (String member : familyMembers) {
-            System.out.println("- " + member);
+        for (int i=0; i<familyMembers.length; i++) {
+            System.out.println("- " + familyMembers[i]);
         }
     }
 }
--- JULY_02/HouseRunner.java
+++ JULY_02/HouseRunner.java
@@ -1,5 +1,6 @@
 public class HouseRunner {
     public static void main(String[] args) {
-        House.displayFamilyDetails("John (Father)", "Jane (Mother)", "Alice (Daughter)", "Bob (Son)");
+		String[] familyMembers= {"John (Father)", "Jane (Mother)", "Alice (Daughter)", "Bob (Son)"};
+        House.displayFamilyDetails(familyMembers);
     }
 }
