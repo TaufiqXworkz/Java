31e08c8c9f8abb4efab586faadf3a8941405ea66
TaufiqXworkz <taufiq.xworkz@gmail.com>
2024-07-15 19:16:47 +0530

    One Program added of JULY 15 TASK

---
+++ JULY_15/ZeroAndOnes.java
@@ -0,0 +1,26 @@
+/* Program:
+[0,1,0,0,1,0,1,0,1]
+write a program to count the 0's and 1's
+in a given array
+*/
+
+public class ZeroAndOnes{
+	
+	public static void main(String args[]){
+		
+		int[] count = {0,1,0,0,1,0,1,0,1};
+		int zeroCount =0;
+		int oneCount =0;
+		
+		for(int index=0 ; index<count.length; index++){
+			if(count[index]==1){
+				oneCount+=1;
+			}else{
+				zeroCount+=1;
+			}
+		}
+		System.out.println("The 1's count is: "+oneCount);
+		System.out.println("The 0's count is: "+zeroCount);
+	}
+
+}
